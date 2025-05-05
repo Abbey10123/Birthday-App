219834203d4583a1fb202e00dddbcacb60e6801a
Abiodun Raheem <abbeyraheem23@gmail.com>
2025-05-05 23:12:22 +0100

    added public schedule endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const UserRoutes = require ("./src/routes/user.route")
 const path = require('path');  
 
 const cors = require('cors');
+const birthdayChecker = require('./src/middlewares/birthdaychecker');
 
 const app = express();
 
@@ -19,6 +20,15 @@ app.get('/bye.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'bye.html'));  
   });
 
+app.get('/run-scheduler', async (req, res) => {
+    try {
+        await birthdayChecker();
+        res.status(200).send("Birthday check run successfully.");
+    } catch (error) {
+        console.error("Failed to run birthday check:", error);
+        res.status(500).send("Error running birthday check.");
+    }
+});  
 
 
 module.exports = app;
