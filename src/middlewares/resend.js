e6b77a0607701a14b8bf97fd4605eb90ecd5f9f9
Abbey <abbeyraheem23@gmail.com>
2025-05-05 23:46:21 +0100

    Update resend.js

--- src/middlewares/resend.js
+++ src/middlewares/resend.js
@@ -1,23 +1,23 @@
-const { Resend } = require('resend');
-const dotenv = require('dotenv');
+// const { Resend } = require('resend');
+// const dotenv = require('dotenv');
 
 
-const resend = new Resend(process.env.Resend);
+// const resend = new Resend(process.env.Resend);
 
 
-const reSender = async (user) => {
-  try {
-    const response = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: user.email,
-      subject: "It's Your Birthday",
-      text: `Hi ${user.userName}, Happy Birthday!`,
-    });
+// const reSender = async (user) => {
+//   try {
+//     const response = await resend.emails.send({
+//       from: 'onboarding@resend.dev',
+//       to: user.email,
+//       subject: "It's Your Birthday",
+//       text: `Hi ${user.userName}, Happy Birthday!`,
+//     });
 
-    console.log('Email sent:', response);
-  } catch (error) {
-    console.error('Failed to send email:', error);
-  }
-};
+//     console.log('Email sent:', response);
+//   } catch (error) {
+//     console.error('Failed to send email:', error);
+//   }
+// };
 
-module.exports = reSender;
+// module.exports = reSender;
